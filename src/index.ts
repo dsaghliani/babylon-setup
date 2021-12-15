7236603b0cf8f6bbeb72348a3ef849ee0370b0ff
Demna Saghliani <saghliani.d@gmail.com>
2021-12-15 18:29:31 +0400

    Disable noResolve and finish up the index.ts boilerplate.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { Engine } from "@babylonjs/core/Engines/engine";
+import { Scene } from "@babylonjs/core/scene";
 
 const canvas: any = document.getElementById("renderCanvas");
 const engine: Engine = new Engine(canvas, true);
+const scene: Scene = new Scene(engine);
+
+engine.runRenderLoop(() => scene.render());
