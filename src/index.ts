4495bb28fb791daa0749a5758576bfdabd7418cb
Demna Saghliani <saghliani.d@gmail.com>
2021-12-15 17:34:28 +0400

    Remove @types/babylonjs from the project and the "types" property from tsconfig.json. Create Engine() instead of BABYLON.Engine().

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { Engine } from "@babylonjs/core/Engines/engine";
 
 const canvas: any = document.getElementById("renderCanvas");
-const engine: Engine = new BABYLON.Engine(canvas, true);
+const engine: Engine = new Engine(canvas, true);
